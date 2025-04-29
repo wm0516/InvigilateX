9393c996c941f9be85821ed6838a750575b7992b
WM <126755724+wm0516@users.noreply.github.com>
2025-04-29 21:23:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from flask import Flask, render_template, request
+import backend
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        text = request.form.get('textbox')
+        return render_template("index.html",
+                               output = backend.meters_feet(float(text)),
+                               user_text = text)
+        
+    if __name__ == "__main__":
+        app.run()
