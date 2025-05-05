9674177432d8d6e36a6aaa81879023963b67fbab
WM <126755724+wm0516@users.noreply.github.com>
2025-05-06 02:22:38 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0efa50f2ad0a21e3fd7e7344d3e48380'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://TomazHayden:roottoor@TomazHayden.mysql.pythonanywhere-services.com/TomazHayden$CourseXcel'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_recycle': 200,
+    'pool_pre_ping': True,
+    'pool_size': 10,
+    'max_overflow': 5
+}
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'minglw04@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jsco bvwc qpor fvku' 
+app.config['MAIL_DEFAULT_SENDER'] = 'minglw04@gmail.com'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
+
+# Create application context before cleanup
+with app.app_context():
+    # Clean up connections
+    db.session.remove()
+    db.engine.dispose()
+
+from main import main
