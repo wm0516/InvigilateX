a3575e9891fa1839936a46ee1a1177f5cada1565
WM <126755724+wm0516@users.noreply.github.com>
2025-05-06 12:24:16 +0800

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-def get_db_connection():
-    return pymysql.connect(
-        host='wmm.mysql.pythonanywhere-services.com',
-        user='wmm',
-        password='Pythonanywhere',
-        database='wmm$InvigilateX_database'
-    )
