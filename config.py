9cbaf66ae68e84f5b7116210f9bad671c9727aa8
WM <126755724+wm0516@users.noreply.github.com>
2025-05-06 12:00:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0efa50f2ad0a21e3fd7e7344d3e48380'
