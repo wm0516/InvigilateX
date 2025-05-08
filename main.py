ace61635daae007f9eaae6dc4a9d17f671773485
WM <126755724+wm0516@users.noreply.github.com>
2025-05-08 14:57:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# check how to do face recognition
+# https://www.youtube.com/watch?v=oDaZrqJ2zoo&list=PLiWNvnK7PSPGPDmrdo3jhi_7hvkGrkFlN&index=3
