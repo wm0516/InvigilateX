5161db8244f48964c209fd474930c387f799db64
WM <126755724+wm0516@users.noreply.github.com>
2025-05-13 21:02:49 +0800

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ class User(db.Model):
     username = db.Column(db.String(100))
     department = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    contact = db.Column(db.String(20))
+    contact = db.Column(db.Integer)
     password = db.Column(db.String(255))
 
     def __init__(self, userid: str, username: str, department: str, email: str, contact: str, password: str):
@@ -22,18 +22,16 @@ class User(db.Model):
         self.contact = contact
         self.password = password
 
-
 class Exam(db.Model):
     __tablename__ = 'Exam'
     examCourseCode = db.Column(db.String(50), primary_key=True)
     examProgram = db.Column(db.String(100))
-    # Default as 1, when over students only will have 2
-    examSection = db.Column(db.String(100))
-    examDate = db.Column(db.Date(100))
-    examTimePeriod = db.Column(db.Time(100))
+    examSection = db.Column(db.String(100))  # Consider limiting to 10 words via validation
+    examDate = db.Column(db.Date)  # Correct date type
+    examTimePeriod = db.Column(db.Time)  # Correct time type
     examLecturer = db.Column(db.String(100))
-    examNoOfCandidates = db.Integer(db.String(100))
-    examVenue = db.Column(db.String(100))
+    examNoOfCandidates = db.Column(db.Integer) 
+    examVenue = db.Column(db.String(100))  # Consider limiting to 10 words via validation
 
 
 
