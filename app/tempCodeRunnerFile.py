f16910553285824e167c64b5297adcd21de9e9d7
WM <126755724+wm0516@users.noreply.github.com>
2025-05-13 22:29:46 +0800

    Delete tempCodeRunnerFile.py

--- app/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-new_user = User(
-                userid = userid_text,
-                username = username_text.upper(),
-                department = department_text,
-                email = email_text,
-                contact = contact_text,
-                password = hashed_pw
-            )
