c63ac722aa72ed84d00598ea698f51de88df8638
WM <126755724+wm0516@users.noreply.github.com>
2025-05-13 22:29:41 +0800

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from app import db
-
+from sqlalchemy.orm import Mapped, mapped_column
 # db.String
 # db.Date -> date format
 # db.Time -> time format
@@ -7,14 +7,16 @@ from app import db
 
 class User(db.Model):
     __tablename__ = 'User'
-    userid = db.Column(db.String(50), primary_key=True)
-    username = db.Column(db.String(100))
-    department = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    contact = db.Column(db.Integer)
-    password = db.Column(db.String(255))
+    # using the mapped[str] to set the variables
+    # Else if not using in this way, need a long code to perform this logic
+    userid: Mapped[str] = mapped_column(db.String(50), primary_key=True)
+    username: Mapped[str] = mapped_column(db.String(100))
+    department: Mapped[str] = mapped_column(db.String(100))
+    email: Mapped[str] = mapped_column(db.String(100))
+    contact: Mapped[int] = mapped_column(db.Integer)
+    password: Mapped[str] = mapped_column(db.String(255))
 
-    def __init__(self, userid: str, username: str, department: str, email: str, contact: str, password: str):
+    def __init__(self, userid, username, department, email, contact, password):
         self.userid = userid
         self.username = username
         self.department = department
@@ -22,16 +24,17 @@ class User(db.Model):
         self.contact = contact
         self.password = password
 
+
 class Exam(db.Model):
     __tablename__ = 'Exam'
     examCourseCode = db.Column(db.String(50), primary_key=True)
     examProgram = db.Column(db.String(100))
-    examSection = db.Column(db.String(100))  # Consider limiting to 10 words via validation
-    examDate = db.Column(db.Date)  # Correct date type
-    examTimePeriod = db.Column(db.Time)  # Correct time type
+    examSection = db.Column(db.String(100))  
+    examDate = db.Column(db.Date)  
+    examTimePeriod = db.Column(db.Time)  
     examLecturer = db.Column(db.String(100))
     examNoOfCandidates = db.Column(db.Integer) 
-    examVenue = db.Column(db.String(100))  # Consider limiting to 10 words via validation
+    examVenue = db.Column(db.String(100)) 
 
 
 
