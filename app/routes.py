09a65224a5d010f2f45ae0334f4743725789309d
WM <126755724+wm0516@users.noreply.github.com>
2025-05-13 23:07:30 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,9 @@ def login_page():
             error_message = result
         else:
             session['user_id'] = result  # Store the user ID in session
-            return redirect(url_for('home_page'))
+            return redirect(url_for('frontPart/home_page'))
 
-    return render_template('login_page.html', login_text=login_text, 
+    return render_template('frontPart/login_page.html', login_text=login_text, 
                            password_text=password_text, error_message=error_message)
 
 
@@ -79,9 +79,9 @@ def register_page():
             db.session.add(new_user)
             db.session.commit()
             flash(f"Register successful! Log in with your registered email address.", "success")
-            return redirect(url_for('login_page'))
+            return redirect(url_for('frontPart/login_page'))
 
-    return render_template('register_page.html', userid_text=userid_text, username_text=username_text, 
+    return render_template('frontPart/register_page.html', userid_text=userid_text, username_text=username_text, 
                            email_text=email_text, contact_text=contact_text, password1_text=password1_text, 
                            password2_text=password2_text, error_message=error_message)
 
@@ -102,9 +102,9 @@ def forgot_password_page():
             if not success:
                 error_message = message
             else:
-                return redirect(url_for('login_page'))
+                return redirect(url_for('frontPart/login_page'))
 
-    return render_template('forgotPassword_page.html', 
+    return render_template('frontPart/forgotPassword_page.html', 
                          forgot_email_text=forgot_email_text, 
                          error_message=error_message)
 
@@ -126,9 +126,9 @@ def reset_password_page(token):
         
         if user and not error_message:
             flash("Password reset successful! Log in with your new password.", "success")
-            return redirect(url_for('login_page'))
+            return redirect(url_for('frontPart/login_page'))
     
-    return render_template('resetPassword_page.html', error_message=error_message)
+    return render_template('frontPart/resetPassword_page.html', error_message=error_message)
 
 
 
@@ -155,4 +155,4 @@ def home_page():
     # flash(f"(flash) Logged in as User ID: {user_id}, User Name: {user.username}" )
 
     
-    return render_template('home_page.html', user_id = user_id, user_name=user_name, user_department=user_department)
+    return render_template('frontPart/home_page.html', user_id = user_id, user_name=user_name, user_department=user_department)
