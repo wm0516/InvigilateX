1c33f273fb5847590f0bf8ea8b86d48c465da4df
WM <126755724+wm0516@users.noreply.github.com>
2025-05-28 19:35:55 +0800

    Update database.py

--- app/database.py
+++ app/database.py
@@ -27,7 +27,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class User(db.Model):
     __tablename__ = 'User'
-
     userid = db.Column(db.String(50), primary_key=True)
     username = db.Column(db.String(100))
     department = db.Column(db.String(100))
