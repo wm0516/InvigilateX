d1a4fd3823686cc70e238976d753a7497a6c63db
WM <126755724+wm0516@users.noreply.github.com>
2025-05-30 00:46:16 +0800

    Update database.py

--- app/database.py
+++ app/database.py
@@ -35,7 +35,7 @@ class User(db.Model):
     password = db.Column(db.String(255))
 
 
-class ExamDetails(db.MModel):
+class ExamDetails(db.Model):
     __tablename__ = 'ExamDetails'
     examDate = db.Column(db.Date, nullable=False)
     examDay = db.Column(db.String(10), nullable=False)
