3aeb1f140ad51b3d916c861a3319b75fc48107c5
WM <126755724+wm0516@users.noreply.github.com>
2025-05-30 22:40:06 +0800

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,34 +5,39 @@ from sqlalchemy.orm import Mapped, mapped_column
 # db.Time -> time format
 # db.Integer -> number
 
-'''class User(db.Model):
-    __tablename__ = 'User'
-    # using the mapped[str] to set the variables
-    # Else if not using in this way, need a long code to perform this logic
-    userid: Mapped[str] = mapped_column(db.String(50), primary_key=True)
-    username: Mapped[str] = mapped_column(db.String(100))
-    department: Mapped[str] = mapped_column(db.String(100))
-    email: Mapped[str] = mapped_column(db.String(100))
-    contact: Mapped[int] = mapped_column(db.Integer)
-    password: Mapped[str] = mapped_column(db.String(255))
+# Admin record after register
+class Admin(db.Model):
+    __tablename__ = 'Admin'
+    adminId = db.Column(db.String(20), primary_key=True)
+    adminName = db.Column(db.String(255))
+    # Department and Level admin set by default as admin
+    adminDepartment = db.Column(db.String(20)) # Level mean access (admin with higher access, level 3 (able to view all dean and lecturer))
+    adminLevel = db.Column(db.String(10))
+    adminEmail = db.Column(db.String(50))
+    adminContact = db.Column(db.Integer)
+    adminPassword = db.Column(db.String(255))
 
-    def __init__(self, userid, username, department, email, contact, password):
-        self.userid = userid
-        self.username = username
-        self.department = department
-        self.email = email
-        self.contact = contact
-        self.password = password'''
+# Dean record after register 
+class Dean(db.Model):
+    __tablename__ = 'Dean'
+    deanId = db.Column(db.String(20), primary_key=True)
+    deanName = db.Column(db.String(255))
+    deanDepartment = db.Column(db.String(20))
+    deanLevel = db.Column(db.Integer) # Level mean access (dean with middle access, level 2 (able to view own lecturer))
+    deanEmail = db.Column(db.String(50))
+    deanContact = db.Column(db.Integer)
+    deanPassword = db.Column(db.String(255))
 
+class Lecturer(db.Model):
+    __tablename__ = 'Lecturer'
+    lecturerId = db.Column(db.String(20), primary_key=True)
+    lecturerName = db.Column(db.String(100))
+    lecturerDepartment =db.Column(db.String(100))
+    lecturerLevel = db.Column(db.Integer) # Level mean access (admin with lower access, level 1 (only able to view own data))
+    lecturerEmail = db.Column(db.String(100))
+    lecturerContact =db.Column(db.Integer)
+    lecturerPassword = db.Column(db.String(255))
 
-class User(db.Model):
-    __tablename__ = 'User'
-    userid = db.Column(db.String(50), primary_key=True)
-    username = db.Column(db.String(100))
-    department = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    contact = db.Column(db.Integer)
-    password = db.Column(db.String(255))
 
 
 # Use examID as PK because the auto increment only able with PK
@@ -63,21 +68,4 @@ class ExamDetails(db.Model):
         examVenue VARCHAR(50)
     );
 
-    '''
-
-class LecturerDetails(db.Model):
-    __tablename__ = 'LecturerDetails'
-    lecturerID = db.Column(db.String(20), primary_key=True)
-    lecturerName = db.Column(db.String(100))
-    lecturerDepartment =db.Column(db.String(100))
-    lecturerEmail = db.Column(db.String(100))
-    lecturerContact =db.Column(db.Integer)
-    '''
-    CREATE TABLE LecturerDetails (  
-        lecturerID VARCHAR(20) PRIMARY KEY,
-        lecturerName VARCHAR(100),
-        lecturerDepartment VARCHAR(100),
-        lecturerEmail VARCHAR(100),
-        lecturerContact INT
-    );
     '''
