7f01e7167653eadb2a8c8389ea654da761b51d07
WM <126755724+wm0516@users.noreply.github.com>
2025-05-30 22:40:04 +0800

    Update adminRoutes.py

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -13,9 +13,11 @@ from itsdangerous import URLSafeTimedSerializer
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 bcrypt = Bcrypt()
 
+# Set the default link into admin_login, because this program have 3 login phase
 @app.route('/')
 def index():
     return redirect(url_for('admin_login'))
+
 
 # login page (done with checking email address and hash password)
 @app.route('/adminLogin', methods=['GET', 'POST'])
@@ -29,70 +31,69 @@ def admin_login():
         login_text = request.form.get('textbox', '').strip()
         password_text = request.form.get('password', '').strip()
 
-        valid, result = check_login(login_text, password_text)
+        valid, result = check_login('admin', login_text, password_text)
         if not valid:
             error_message = result
         else:
-            session['user_id'] = result  # Store the user ID in session
+            session['admin_id'] = result  # Store the user ID in session
             return redirect(url_for('admin_homepage'))
 
-    return render_template('adminPart/adminLogin.html', login_text=login_text, password_text=password_text, error_message=error_message)
+    return render_template('adminPart/adminLogin.html', error_message=error_message)
+
 
 # register page (done with all input validation and userID as Primary Key)
 @app.route('/adminRegister', methods=['GET', 'POST'])
 def admin_register():
-    userid_text = ''
-    username_text = ''
-    department_text = ''
-    email_text = ''
-    contact_text = ' '
-    password1_text = ''
-    password2_text = ''
+    adminId_text = ''
+    adminnNme_text = ''
+    adminEmail_text = ''
+    adminContact_text = ' '
+    adminPassword1_text = ''
+    adminPassword2_text = ''
     error_message = None
 
     if request.method == 'POST':
-        userid_text = request.form.get('userid', '').strip()
-        username_text = request.form.get('username', '').strip()
-        department_text = request.form.get('department', '').strip()
-        email_text = request.form.get('email', '').strip()
-        contact_text = request.form.get('contact', '').strip()
-        password1_text = request.form.get('password1', '').strip()
-        password2_text = request.form.get('password2', '').strip()
+        adminId_text = request.form.get('userid', '').strip()
+        adminnNme_text = request.form.get('username', '').strip()
+        adminEmail_text = request.form.get('email', '').strip()
+        adminContact_text = request.form.get('contact', '').strip()
+        adminPassword1_text = request.form.get('password1', '').strip()
+        adminPassword2_text = request.form.get('password2', '').strip()
 
         # Use the new check_register function
-        is_valid, error_message = check_register(userid_text, email_text, contact_text)
+        is_valid, error_message = check_register('admin', adminId_text, adminEmail_text, adminContact_text)
         
         if not is_valid:
             pass  # error_message is already set
-        elif not all([userid_text, username_text, department_text, email_text, contact_text]):
+        elif not all([adminId_text, adminnNme_text, adminEmail_text, adminContact_text]):
             error_message = "All fields are required."
-        elif not email_format(email_text):
+        elif not email_format(adminEmail_text):
             error_message = "Wrong Email Address format"
-        elif not contact_format(contact_text):
+        elif not contact_format(adminContact_text):
             error_message = "Wrong Contact Number format"
-        elif password1_text != password2_text:
+        elif adminPassword1_text != adminPassword2_text:
             error_message = "Passwords do not match."
-        elif not password_format(password1_text):
+        elif not password_format(adminPassword1_text):
             error_message = "Wrong password format."
         else:
-            hashed_pw = bcrypt.generate_password_hash(password1_text).decode('utf-8')
-            new_user = User(
-                userid = userid_text,
-                username = username_text.upper(),
-                department = department_text,
-                email = email_text,
-                contact = contact_text,
-                password = hashed_pw
+            hashed_pw = bcrypt.generate_password_hash(adminPassword1_text).decode('utf-8')
+            new_admin = Admin(
+                adminId = adminId_text,
+                adminName = adminnNme_text.upper(),
+                adminDepartment = 'Admin',
+                adminLevel = '3',
+                adminEmail = adminEmail_text,
+                adminContact = adminContact_text,
+                adminPassword = hashed_pw
             )
             
-            db.session.add(new_user)
+            db.session.add(new_admin)
             db.session.commit()
             flash(f"Register successful! Log in with your registered email address.", "success")
             return redirect(url_for('admin_login'))
 
-    return render_template('adminPart/adminRegister.html', userid_text=userid_text, username_text=username_text, 
-                           email_text=email_text, contact_text=contact_text, password1_text=password1_text, 
-                           password2_text=password2_text, error_message=error_message)
+    return render_template('adminPart/adminRegister.html', error_message=error_message)
+
 
 # forgot password page (done when the email exist in database will send reset email link)
 @app.route('/adminForgotPassword', methods=['GET', 'POST'])
@@ -106,15 +107,14 @@ def admin_forgotPassword():
         if not forgot_email_text:
             error_message = "Email address is required."
         else:
-            success, message = check_forgotPasswordEmail(forgot_email_text)
+            success, message = check_forgotPasswordEmail('admin', forgot_email_text)
             if not success:
                 error_message = message
             else:
                 return redirect(url_for('admin_login'))
 
-    return render_template('adminPart/adminForgotPassword.html', 
-                         forgot_email_text=forgot_email_text, 
-                         error_message=error_message)
+    return render_template('adminPart/adminForgotPassword.html', error_message=error_message)
+
 
 # reset password page (done after reset password based on that user password)
 @app.route('/adminResetPassword/<token>', methods=['GET', 'POST'])
@@ -124,18 +124,14 @@ def admin_resetPassword(token):
     if request.method == 'POST':
         password_text_1 = request.form.get('password1', '').strip()
         password_text_2 = request.form.get('password2', '').strip()
-        
-        user, error_message = check_resetPassword(
-            token, 
-            password_text_1, 
-            password_text_2
-        )
-        
-        if user and not error_message:
+
+        admin, error_message = check_resetPassword('admin', token, password_text_1, password_text_2)
+        if admin and not error_message:
             flash("Password reset successful! Log in with your new password.", "success")
             return redirect(url_for('admin_login'))
-    
+        
     return render_template('adminPart/adminResetPassword.html', error_message=error_message)
+
 
 # Logout button from homepage to login page
 @app.route('/adminLogout')
@@ -146,33 +142,28 @@ def admin_logout():
     return redirect(url_for('admin_login')) 
 
 
-
-
-
-
-
-
-
 # Once login sucessful, it will kept all that user data and just use when need
 @app.context_processor
 def inject_user_data():
-    user_id = session.get('user_id')
-    if user_id:
-        user = User.query.get(user_id)
-        if user:
+    adminId = session.get('admin_id')
+    if adminId:
+        admin = Admin.query.get(adminId)
+        if admin:
             return {
-                'user_id': user_id,
-                'user_name': user.username,
-                'user_department': user.department,
-                'user_email': user.email,
-                'user_contact': user.contact
+                'admin_id': adminId,
+                'admin_name': admin.adminName,
+                'admin_department': admin.adminDepartment,
+                'admin_level': admin.adminLevel,
+                'admin_email': admin.adminEmail,
+                'admin_contact' : admin.adminContact
             }
     return {
-        'user_id': None,
-        'user_name': '',
-        'user_department': '',
-        'user_email': '',
-        'user_contact': ''
+        'admin_id': None,
+        'admin_name': '',
+        'admin_department': '',
+        'admin_level': '',
+        'admin_email': '',
+        'admin_contact': '' 
     }
 
 
@@ -248,10 +239,11 @@ def admin_uploadLecturerTimetable():
                                 errors.append(f"Row {row_number} in sheet '{sheet_name}' error: {error_message}")
                                 continue
                             
-                            lecturer = LecturerDetails(
+                            lecturer = Lecturer(
                                 lecturerID = lecturer_id,
                                 lecturerName = lecturer_name,
                                 lecturerDepartment = lecturer_department,
+                                lecturerLevel = 'Lecturer', # LecturerLevel is set by default
                                 lecturerEmail = lecturer_email,
                                 lecturerContact = lecturer_contact
                             )
