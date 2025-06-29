d3c49201ffc98e3bb71e2d4ce10f84773d9a841d
WM <126755724+wm0516@users.noreply.github.com>
2025-06-30 02:03:07 +0800

    Update adminRoutes.py

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -366,8 +366,6 @@ def admin_manageExam():
                         print(f"Raw columns from sheet '{sheet_name}': {df.columns.tolist()}")
 
                          # Clean and standardize columns
-                        df.columns = [str(col).strip().lower() for col in df.columns]
-                        print("Detected columns:", df.columns.tolist())
                         expected_cols = ['Date', 'Day', 'Start', 'End', 'Program', 'Course/Sec', 'Lecturer', 'No Of', 'Room']
 
                         if df.columns.tolist() != expected_cols:
