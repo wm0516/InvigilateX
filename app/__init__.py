10467ef2ae55605490f1fc0a713a8cd7154f6ee7
WM <126755724+wm0516@users.noreply.github.com>
2025-08-14 23:16:57 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 
-from app import auth
+from app import app
 
 # Create the Flask application
 app = Flask(__name__)
@@ -26,7 +26,6 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'minglw04@gmail.com'
 app.config['MAIL_PASSWORD'] = 'jsco bvwc qpor fvku'
 app.config['MAIL_DEFAULT_SENDER'] = 'minglw04@gmail.com'
-
 
 # Initialize extensions
 db = SQLAlchemy(app)
