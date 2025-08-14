f01dd0a0a4b1d7f1f0532155948d75486d2c54af
WM <126755724+wm0516@users.noreply.github.com>
2025-08-14 14:59:11 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -759,6 +759,7 @@ def admin_manageExam():
 @app.route('/get_lecturers_by_department/<department_code>')
 def get_lecturers_by_department(department_code):
     # Ensure case-insensitive match if needed
+    print(f"User Department Code is: {department_code}")
     lecturers = User.query.filter_by(userDepartment=department_code, userLevel=1).all()
     lecturers_list = [{"userName": l.userName} for l in lecturers]
     return jsonify(lecturers_list)
