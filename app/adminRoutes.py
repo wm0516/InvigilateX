1aad03c2cb995ea23bab634c999a1796c6599b83
WM <126755724+wm0516@users.noreply.github.com>
2025-08-21 22:48:21 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -592,10 +592,23 @@ def admin_manageCourse():
 # Function for admin to manage exam information (adding, editing, and removing)
 @app.route('/admin/manageExam', methods=['GET', 'POST'])
 def admin_manageExam():
-    exam_data = Exam.query.filter(Exam.examStartTime.isnot(None), Exam.examEndTime.isnot(None)).all() # Display out only with value data, null value data will not be displayed out 
     department_data = Department.query.all() # For department code dropdown
     venue_data = Venue.query.filter(Venue.venueStatus == 'AVAILABLE').all() # For venue selection dropdown
-    course_data = Course.query.join(Exam).filter(Exam.examStartTime.isnot(None), Exam.examEndTime.isnot(None)).all()  # For course selection dropdown and show out related tutorial, practical, and number of students
+    exam_data = Exam.query.filter(
+        or_(
+            Exam.examStartTime.is_(None),
+            Exam.examEndTime.is_(None)
+        )
+    ).all() # Display out only with value data, null value data will not be displayed out 
+
+    course_data = (Course.query.join(Exam)  # assumes Course has relationship with Exam
+        .filter(
+            or_(
+                Exam.examStartTime.is_(None),
+                Exam.examEndTime.is_(None)
+            )
+        ).all()
+    ) # For course selection dropdown and show out related tutorial, practical, and number of students
 
     # Default values for manual form
     examDate_text = ''
