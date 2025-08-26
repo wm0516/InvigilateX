e34997a1e410404fe8c51f5c7dca85c6e0553cb1
WM <126755724+wm0516@users.noreply.github.com>
2025-08-27 02:52:01 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -753,10 +753,6 @@ def get_course_details(program_code, course_code_section):
 
 
 
-
-
-
-
 def get_drive_service():
     SERVICE_ACCOUNT_FILE = '/home/WM05/xenon-chain-460911-p8-0931c798d991.json'
     SCOPES = ['https://www.googleapis.com/auth/drive']
