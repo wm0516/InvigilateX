95924564b16def4a9d40192b74135ce53f8a3dcb
wm law <minglw04@gmail.com>
2025-09-16 02:38:18 +0800

    Update adminRoutes.py

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -1141,7 +1141,7 @@ def admin_manageTimetable():
         return redirect(url_for('admin_manageTimetable'))
     
     return render_template(
-        "admin/admminManageTimetable.html",
+        "admin/adminManageTimetable.html",
         timetable_data=timetable_data,
         files=files,
         lecturers=lecturers,
