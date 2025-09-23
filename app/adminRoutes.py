cff18edc6e5fab05e77dc11b938019538ed00383
wm law <minglw04@gmail.com>
2025-09-24 03:29:28 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -1237,7 +1237,7 @@ def admin_manageTimetable():
         elif form_type == 'edit':
             action = request.form.get('action')
             if action == 'update' and timetable_select:
-                timetable_select.staffId = request.form['editStaffList']
+                timetable_select.user_id = request.form['editStaffList']
                 db.session.commit()
                 flash("Timetable updated successfully.", "success")
 
