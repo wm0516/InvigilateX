2743e450271a8e238a882bbeb2e1ab1b0446a0b8
wm law <minglw04@gmail.com>
2025-10-01 01:36:19 +0800

    Update deanRoutes.py

--- app/deanRoutes.py
+++ app/deanRoutes.py
@@ -44,14 +44,6 @@ def dean_timetable():
 @app.route('/dean/mergeTimetable', methods=['GET', 'POST'])
 def dean_mergeTimetable():
     deanId = session.get('user_id')
-    timetable = Timetable.query.filter_by(user_id.userDepartment=deanId.userDepartment).first()
-    timetable_rows = timetable.rows if timetable else []
-    return render_template('dean/deanMergeTimetable.html', active_tab='dean_mergeTimetabletab', timetable_rows=timetable_rows) #, timetable=timetable)
-
-
-@app.route('/dean/mergeTimetable', methods=['GET', 'POST'])
-def dean_mergeTimetable():
-    deanId = session.get('user_id')
     timetable = Timetable.query.join(User).filter(User.userDepartment == deanId.userDepartment).all()
     
     # Flatten rows from all matching timetables
