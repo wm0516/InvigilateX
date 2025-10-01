31062aa4c6a9fd538d60dcab40dccebc4149b702
wm law <minglw04@gmail.com>
2025-10-02 02:10:20 +0800

    Update adminRoutes.py

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -184,7 +184,7 @@ def get_courseCodeSection(courseCodeSection_select):
 @app.route('/admin/manageCourse', methods=['GET', 'POST'])
 def admin_manageCourse():
     # === Load basic data safely ===
-    course_data = Course.query.order_by(Course.courseStatus.desc()).all()
+    course_data = Course.query.order_by(Course.courseStatus.desc(), Course.courseDepartment.asc()).all()
     department_data = Department.query.all()
 
     course_id = request.form.get('editCourseSelect')
@@ -704,7 +704,7 @@ def admin_manageExam():
 
     # Base query: only exams whose course is active
     exam_data_query = Exam.query.join(Exam.course).filter(Course.courseStatus == True)
-    exam_data = exam_data_query.order_by(Exam.examId.asc()).all()
+    exam_data = exam_data_query.order_by(Exam.examId.asc(),Exam.examStartTime.asc(),Exam.examVenue.asc()).all()
 
     # For Edit section
     exam_selected = request.form.get('editExamCourseSection')
