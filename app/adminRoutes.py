4de338476aeafe5e5b3d37e0f191f812cfc71198
wm law <minglw04@gmail.com>
2025-10-11 16:32:43 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -1711,7 +1711,7 @@ def admin_manageTimetable():
         TimetableRow.query
         .join(Timetable, TimetableRow.timetable_id == Timetable.timetableId)
         .join(User, Timetable.user_id == User.userId)
-        .filter(User.userStatus == True)  # active staff only
+        .filter(User.userStatus == 1)  # active staff only
     )
 
     # Department filter
