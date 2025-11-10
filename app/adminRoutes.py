776da144878f54790e1fdcd2bf6c4e5a96155951
wm law <minglw04@gmail.com>
2025-11-10 23:43:39 +0800

    update

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -2152,18 +2152,11 @@ def admin_manageInvigilationReport():
     for att in attendances:
         report = att.report
         exam = Exam.query.get(report.examId) if report else None
-        att.group_key = (not exam.examStatus if exam else True, exam.examStartTime if exam else datetime.min)
-        flash(f"Attendance: {exam}", "success") 
         att.group_key = (
             not exam.examStatus if exam else True,
             exam.examStartTime if exam else datetime.min,
             exam.examId if exam else 0
         )
-    
-    reports_with_attendance = db.session.query(InvigilationReport).join(InvigilatorAttendance).count()
-    all_reports = InvigilationReport.query.count()
-    flash(f"All Reports: {all_reports}", "success")
-    flash(f"Reports With Attendance: {reports_with_attendance}", "success")
 
     if request.method == 'POST':
         form_type = request.form.get('form_type')
