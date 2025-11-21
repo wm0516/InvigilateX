32e6e3526f8285ccd1e35b450751b1be7b14ea71
wm law <minglw04@gmail.com>
2025-11-22 02:05:03 +0800

    Update authRoutes.py

--- app/authRoutes.py
+++ app/authRoutes.py
@@ -129,6 +129,7 @@ def register():
                 userPassword=hashed_pw,
                 userGender=gender_text,
                 userStatus=0,  # not verified yet
+                userCardId=card_text,
                 userRegisterDateTime=datetime.now() + timedelta(hours=8)
             )
             db.session.add(new_user)
