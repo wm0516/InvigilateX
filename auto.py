9a77b531ab6ed2c94b47137827581b1c6b4881c2
wm law <minglw04@gmail.com>
2025-12-15 02:06:10 +0800

    update

--- auto.py
+++ auto.py
@@ -3,4 +3,5 @@ from app.backend import send_invigilator_slot_notifications_for_all
 
 if __name__ == "__main__":
     with app.app_context():
-        send_invigilator_slot_notifications_for_all()
+        results = send_invigilator_slot_notifications_for_all()
+        print(results)  # <-- add this line to see which emails were sent
