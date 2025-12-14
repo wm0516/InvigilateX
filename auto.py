f97c62a7c661fb7f6a6583e63f5691ddca2873a9
wm law <minglw04@gmail.com>
2025-12-15 02:04:34 +0800

    update

--- auto.py
+++ auto.py
@@ -1,5 +1,5 @@
 from app import app
-from backend import send_invigilator_slot_notifications_for_all
+from app.backend import send_invigilator_slot_notifications_for_all
 
 if __name__ == "__main__":
     with app.app_context():
