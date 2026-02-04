c7e1fa88c3e76a13ad3ab3f0d6923fb6666665a6
wm law <minglw04@gmail.com>
2026-02-05 01:40:26 +0800

    .

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -230,7 +230,6 @@ def admin_manageCourse():
     # === Get all courses, optionally filtered by intake ===
     course_query = Course.query.order_by(
         Course.courseStatus.desc(),
-        Course.courseDepartment.asc(),
         Course.courseCodeSectionIntake.asc()
     )
     course_data = course_query.all()
