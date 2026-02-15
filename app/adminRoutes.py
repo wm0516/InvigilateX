ac64c5eeead234a50f3182d36a835a64298a6f4e
wm law <minglw04@gmail.com>
2026-02-16 01:28:24 +0800

    Update adminRoutes.py

--- app/adminRoutes.py
+++ app/adminRoutes.py
@@ -2083,10 +2083,10 @@ def get_session_details(session_id):
 
     # Fetch backup invigilator
     backup_invigilator = None
-    if session.backupInvigilator:
+    if session.backupInvigilatorId:
         backup_invigilator = {
-            "invigilatorId": session.backupInvigilator.userId,
-            "invigilatorName": session.backupInvigilator.userName
+            "invigilatorId": session.backupInvigilatorId.userId,
+            "invigilatorName": session.backupInvigilatorId.userName
         }
 
     return jsonify({
@@ -2095,7 +2095,7 @@ def get_session_details(session_id):
         "start": session.startDateTime.strftime("%d/%b/%Y %H:%M"),
         "end": session.endDateTime.strftime("%d/%b/%Y %H:%M"),
         "attendances": attendances,
-        "backupInvigilator": backup_invigilator
+        "backupInvigilatorId": backup_invigilator
     })
 
 
@@ -2161,8 +2161,10 @@ def admin_manageInvigilationReport():
 
         grouped_att[key]["invigilators"].append(vsi)
 
-        if venue_session.backupInvigilator:
-            grouped_att[key]["backup"] = venue_session.backupInvigilator
+        if venue_session.backupInvigilatorId:
+            grouped_att[key]["backup"] = venue_session.backupInvigilatorId
+            flash(f"Here is the: {venue_session.backupInvigilatorId}", "success")
+            print(f"Here is the: {venue_session.backupInvigilatorId}")
 
         for ve in venue_session.exams:
             if ve.exam and ve.exam.course:
