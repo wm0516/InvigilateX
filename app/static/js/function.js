304bbf1ec1c64d73c682190bce2548a1cc32ee9c
wm law <minglw04@gmail.com>
2026-02-26 02:45:20 +0800

    .

--- app/static/js/function.js
+++ app/static/js/function.js
@@ -189,7 +189,7 @@ function searchContent() {
     const filter = input.value.toLowerCase();
     let anyVisible = false;
 
-    const tables = ["manageCourse","manageDepartment","manageVenue","manageExam","manageStaff","viewStaff","activity","manageAccess"];
+    const tables = ["manageCourse","manageDepartment","manageVenue","manageExam","manageStaff","viewStaff","activity","access"];
     for (const tableId of tables) {
         const table = document.getElementById(tableId);
         if (!table) continue;
